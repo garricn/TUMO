7b388623e031dc3ebf36bb69468ccd26b8c39489
Garric G. Nahapetian <garricn@icloud.com>
2017-07-12 19:26:56 +0400

    wip

--- TUMO/MyViewController.swift
+++ TUMO/MyViewController.swift
@@ -116,29 +116,6 @@ class MyViewController: UITableViewController, UISearchBarDelegate {
 }
 
 
-class MyTableViewCell: UITableViewCell {
-
-    let myImageView = UIImageView()
-    
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-        contentView.addSubview(myImageView)
-
-        myImageView.contentMode = .scaleAspectFit
-        myImageView.translatesAutoresizingMaskIntoConstraints = false
-
-        myImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-        myImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-        myImageView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        myImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-}
 
 
 
