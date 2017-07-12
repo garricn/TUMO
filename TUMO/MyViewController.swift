df0a7b943636a8b92e6da0ed64ff2628b6c13c63
Garric G. Nahapetian <garricn@icloud.com>
2017-07-12 16:21:46 +0400

    more search

--- TUMO/MyViewController.swift
+++ TUMO/MyViewController.swift
@@ -12,29 +12,36 @@ class MyViewController: UITableViewController, UISearchBarDelegate {
 
     let searchController = UISearchController(searchResultsController: nil)
 
-    var names = ["Garen", "Garric", "Hayk", "Ani", "Lilit"]
+    let names = ["Garen", "Garric", "Hayk", "Ani", "Lilit"]
+
+    var items: [String] = []
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if let searchText = searchBar.text {
-//            names = names.filter { name in
-//                return name.lowercased().contains(searchText.lowercased())
-//            }
-
-            var filterdNames: [String] = []
-
-            for name in names {
-                if name.lowercased().contains(searchText.lowercased()) {
-                    filterdNames.append(name)
-                }
+            items = items.filter {
+                $0.lowercased().contains(searchText.lowercased())
             }
 
-            names = filterdNames
-
             tableView.reloadData()
+            searchController.dismiss(animated: true, completion: nil)
         }
     }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        reset()
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            reset()
+            searchController.dismiss(animated: true, completion: nil)
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        items = names.sorted()
 
         searchController.searchBar.delegate = self
         tableView.tableHeaderView = searchController.searchBar
@@ -48,24 +55,38 @@ class MyViewController: UITableViewController, UISearchBarDelegate {
 
         navigationItem.rightBarButtonItem = myBarButtonItem
 
-        names.sort()
+        let resetButton = UIBarButtonItem(
+            barButtonSystemItem: .refresh,
+            target: self,
+            action: #selector(didTapResetButton))
+
+        navigationItem.leftBarButtonItem = resetButton
+    }
+
+    func reset() {
+        items = names.sorted()
+        tableView.reloadData()
+    }
+
+    func didTapResetButton(sender: UIBarButtonItem) {
+        reset()
     }
 
     func didTapAddButton(sender: UIBarButtonItem) {
         let newString = "Item \(names.count)"
-        names.append(newString)
+        items.append(newString)
         tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return items.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = items[indexPath.row]
         return cell
     }
 
@@ -75,7 +96,7 @@ class MyViewController: UITableViewController, UISearchBarDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
         let viewController = UIViewController()
         viewController.view.backgroundColor = .white
-        viewController.title = names[indexPath.row]
+        viewController.title = items[indexPath.row]
         navigationController?.pushViewController(viewController, animated: true)
     }
 }
