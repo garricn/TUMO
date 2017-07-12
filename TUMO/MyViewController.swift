9055e1028337c12b41de0a4b2eb4df8581858bfb
Garric G. Nahapetian <garricn@icloud.com>
2017-07-12 14:12:10 +0400

    insert row and add search bar

--- TUMO/MyViewController.swift
+++ TUMO/MyViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class MyViewController: UITableViewController {
+class MyViewController: UITableViewController, UISearchBarDelegate {
 
-    var names = ["Garen", "Garric", "Hayk", "Ani", "Lilit"]
+    let names = ["Garen", "Garric", "Hayk", "Ani", "Lilit"]
+
+    var items: [String] = []
+
+    let searchController = UISearchController.init(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,27 +28,33 @@ class MyViewController: UITableViewController {
 
         navigationItem.rightBarButtonItem = myBarButtonItem
 
-        names.sort()
+        items = names.sorted()
+
+
+        searchController.searchBar.delegate = self
+        tableView.tableHeaderView = searchController.searchBar
     }
 
     func didTapAddButton(sender: UIBarButtonItem) {
-        let newString = "Item \(names.count)"
-        names.append(newString)
-        tableView.reloadData()
+        let newString = "Item \(items.count)"
+        items.append(newString)
+        let indexPath = IndexPath(row: items.count - 1, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+//        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return names.count
+        return items.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
 
-        cell.textLabel?.text = names[indexPath.row].uppercased()
+        cell.textLabel?.text = items[indexPath.row].uppercased()
 
         return cell
     }
@@ -55,8 +65,14 @@ class MyViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
         let viewController = UIViewController()
         viewController.view.backgroundColor = .white
-        viewController.title = names[indexPath.row]
+        viewController.title = items[indexPath.row]
         navigationController?.pushViewController(viewController, animated: true)
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        items = names.filter { $0.contains(searchBar.text!) }
+        tableView.reloadData()
+        searchController.dismiss(animated: true, completion: nil)
     }
 }
 
